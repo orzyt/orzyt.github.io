17d597c125cde5759256b124a1359fb704257e8e
orzyt <xmwzzyt@qq.com>
2019-03-29 22:03:54 +0800

    Site updated: 2019-03-29 22:03:53

--- js/main.js
+++ js/main.js
@@ -1,39 +1,16 @@
 (function($){
     var toTop = ($('#sidebar').height() - $(window).height()) + 60;
     // Caption
-    
     $('.article-entry').each(function(i) {
         $(this).find('img').filter(function (element) {
             return $(this).hasClass('');
         }).each(function() {
-            if (this.alt && !(!!$.prototype.justifiedGallery && $(this).parent('.justified-gallery').length)) {
+            if (this.alt) {
                 $(this).after('<span class="caption">' + this.alt + '</span>');
             }
-
-            // 对于已经包含在链接内的图片不适用lightGallery
-            if ($(this).parent().prop("tagName") !== 'A') {
-                $(this).wrap('<div align="center"></div>');
-            }
+            $(this).wrap('<div align="center"></div>');
         });
     });
-     
-    // if (typeof lightGallery != 'undefined') {
-    //     var options = {
-    //         selector: '.gallery-item'
-    //     };
-    //     $('.article-entry').each(function(i, entry) {
-    //         lightGallery(entry, options);
-    //     });
-    //     lightGallery($('.article-gallery')[0], options);
-    // }
-    // if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
-    //     var options = {
-    //         rowHeight: 140,
-    //         margins: 4,
-    //         lastRow: 'justify'
-    //     };
-    //     $('.justified-gallery').justifiedGallery(options);
-    // }
 
     // Profile card
     $(document).on('click', function () {
@@ -146,3 +123,94 @@ $(document).ready(function()
     }, 100);
 })(jQuery);
 
+// var $itemHasChild = $("#toc .toc-item:has(> .toc-child)");
+// var $titleHasChild = $itemHasChild.children(".toc-link");
+// $itemHasChild.prepend("<i class='fa fa-caret-down'></i><i class='fa fa-caret-right'></i>");
+// var $iconToFold = $(".toc-item > .fa-caret-down");
+// $iconToFold.addClass("hide");
+
+// var $x = $("#toc .toc-item > a");
+// $x.siblings(".toc-child").toggle(false);
+
+// var clickIcon = function(){
+//     $("#toc .toc-item > i").click(function(){
+//         $(this).siblings(".toc-child").slideToggle(100);
+//         $(this).toggleClass("hide");
+//         $(this).siblings("i").toggleClass("hide");
+//     })
+// }()
+
+
+var $w = $(window);
+// 修正响应不及时的问题
+var HEADFIX = 30;
+
+var $toclink = $('.toc-link'),
+  $headerlink = $('.headerlink'),
+  $tocchild = $('.toc-child');
+
+// 用来获取 top 数组
+var headerlinkTop = [];
+headerlinkTop = $.map($headerlink, function(link) {
+  return $(link.parentNode).offset().top - HEADFIX;
+});
+
+headerlinkTop[0] = -1;
+headerlinkTop.push(Infinity);
+
+var pos = 0;
+var getActive = function(s_top){
+
+  for(var i = 0; i < $toclink.length; i++) {
+    var currentTop = headerlinkTop[i];
+    var nextTop = headerlinkTop[i+1];
+
+    if(s_top > currentTop && s_top <= nextTop){
+      $toclink.removeClass('active');
+      $toclink.siblings(".toc-child").toggle(false);
+      pos = i;
+      var nowlink = $toclink[i];
+      $(nowlink).addClass('active');
+      $(nowlink).siblings(".toc-child").toggle(true);
+      
+      while($(nowlink.parentNode.parentNode).hasClass('toc-child')){
+        nowlink = nowlink.parentNode.parentNode;
+        $(nowlink).toggle(true);
+      }
+      break;
+    }
+  }
+}
+getActive($w.scrollTop());
+
+// 修复 image load bug
+// 由于网页已经加载，而图片还未加载完成，导致数组的高度有偏差
+var link_length = $headerlink.length;
+var $link_last = link_length > 1 ? $($headerlink[$headerlink.length - 1].parentNode) : null;
+var fixLoading = function(){
+  if(link_length > 1){
+    if(($link_last.offset().top - HEADFIX) - headerlinkTop[link_length - 1] != 0){
+      headerlinkTop = $.map($headerlink, function(link) {
+        return $(link.parentNode).offset().top - HEADFIX;
+      });
+      // 修正参数
+      headerlinkTop[0] = -1;
+      headerlinkTop.push(Infinity);
+    }
+  }
+}
+
+// callback
+var cb = function(){
+  var scrollTop = $w.scrollTop();
+  fixLoading();
+  if(scrollTop > headerlinkTop[pos + 1] || scrollTop <= headerlinkTop[pos]){
+    getActive(scrollTop);
+  }
+}
+
+if(!this.doScroll){
+  this.doScroll = true;
+  // 监听浏览器滚动
+  $w.scroll(cb);
+}
